a2ae5ee1cade14b53423140d5b1d0b7d440c73e4
Karthik <154497325+Kathrojukarthik@users.noreply.github.com>
2025-08-12 16:06:49 +0530

    Create searchinrotated.java

---
+++ arrays/recursion/searchinrotated.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return searchtar(nums,target,0,nums.length-1);
+    }
+    static int searchtar(int[] nums, int target, int s,int e){
+        if(s > e){
+            return -1;
+        }
+        int mid=s+(e-s)/2;
+        if(target==nums[mid]){
+            return mid;
+        }
+        if(nums[s] <=nums[mid]){
+            if(target >=nums[s] && target <= nums[mid]){
+                return searchtar(nums,target,s,mid-1);
+            }else{
+                return searchtar(nums,target,mid+1,e);
+            }
+        }
+        if(target >=nums[mid] && target <=nums[e]){
+                return searchtar(nums,target,mid+1,e);
+        }
+        return searchtar(nums,target,s,mid-1);
+    }
+}
